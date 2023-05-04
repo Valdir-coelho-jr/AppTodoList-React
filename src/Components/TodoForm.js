4614497e6c22c9c726dde84e92490e413b746e8c
Valdir Coelho Jr <valdircoelho.jr@gmail.com>
2023-05-04 19:44:44 -0300

    Initial Commit

---
+++ src/Components/TodoForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    addTodo(value);
+
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        value={value}
+        type="text"
+        className="todo-input"
+        placeholder="Quais são as tarefas de hoje?"
+        onChange={(e) => setValue(e.target.value)}
+      ></input>
+      <button type="submit" className="todo-btn">
+        Add Tarefa
+      </button>
+    </form>
+  );
+};
